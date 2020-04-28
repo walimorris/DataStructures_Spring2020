bcb561165b91cb68be897577211f87614572bea2
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-04-27 23:07:06 -0700

    Update ListsSetsMaps.java

--- CH11_Lists,Sets,Maps/ListsSetsMaps.java
+++ CH11_Lists,Sets,Maps/ListsSetsMaps.java
@@ -70,3 +70,69 @@ public class ModifiedSieve {
         return primes; 
     } 
 }     
+
+/**
+ * Exercise 2 
+ *
+ * Write a method called alternate that accepts two Lists as its parameters and returns a new List
+ * containing alternating elements from the two lists, in order: first element from first list, 
+ * second element from second list, etc
+ *
+ * If the lists do not contain the same number of elements, the remaining elements from the longer 
+ * list should be placed consecutively at the end. 
+ *
+ * @author Wali Morris 
+ * @since 04/27/2020 
+ */
+
+import java.util.*;
+
+public class Alternate {
+    public static void main(String[] args) {
+        List<Integer> input1 = new LinkedList<>(
+                        List.of(1, 2, 3, 4, 5));
+        List<Integer> input2 = new LinkedList<>(
+                        List.of(6, 7, 8, 9, 10, 11, 12));
+        System.out.print(input1 + " + " + input2 + " -> ");
+        List<Integer> output1 = alternate(input1, input2);
+        System.out.println(output1); // input1 and input2
+        List<Integer> input3 = new LinkedList<>(
+                        List.of(1, 2, 3, 4, 5, 6, 7, 8));
+        List<Integer> input4 = new LinkedList<>(
+                        List.of(9, 10, 11, 12));
+        System.out.print(input3 + " + " + input4 + " -> ");
+        List<Integer> output2 = alternate(input3, input4);
+        System.out.println(output2); // input3 and input4
+        List<Integer> input5 = new LinkedList<>(
+                        List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> input6 = new LinkedList<>(
+                        List.of(7, 8, 9, 10, 11, 12));
+        System.out.print(input5 + " + " + input6 + " -> ");
+        List<Integer> output3 = alternate(input5, input6);
+        System.out.println(output3);
+    }
+	
+    public static List<Integer> alternate(List<Integer> list1,
+                    List<Integer> list2) {
+        /* each element in Lists will be appended to numbers list */
+        List<Integer> numbers = new LinkedList<>();
+        /* Until either list is empty, alternate appending number from list1 then 
+         * list2. Once any list becomes empty, the alternating of numbers and this
+         * loop stops */
+        while (!list1.isEmpty() && !list2.isEmpty() ) {
+            int num1 = list1.remove(0);
+            int num2 = list2.remove(0);
+            numbers.add(num1);
+            numbers.add(num2);
+        }
+        /* check to see which list is empty and append the non-empty list to the end 
+         * of numbers list. If both list are empty, there is nothing to append and 
+         * numbers is returned */
+        if ( list1.isEmpty() ) {
+            numbers.addAll(list2);
+        } else {
+            numbers.addAll(list1);
+        }
+        return numbers;
+    }
+}
