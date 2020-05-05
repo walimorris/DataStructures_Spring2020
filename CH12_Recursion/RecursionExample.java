862d49c546a70a9bc41389e5764b32f5670eb8f9
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-05-05 11:48:04 -0700

    Add files via upload

---
+++ CH12_Recursion/RecursionExample.java
@@ -0,0 +1,29 @@
+/**
+ * Example
+ *
+ * A try at defining a recursive method before starting Chapter 12 reading and exercises 
+ * 
+ * NOTE: There's a really good chapter on recursion in GEB by @Douglas R. Hofstadter
+ * to read and review 
+ *
+ * @author Wali Morris 
+ * @since 05/05/2020
+ */
+
+import java.util.*; 
+
+public class RecursionExample { 
+    public static void main(String[] args) { 
+	toZero(100); 
+    } 
+
+    public static void toZero(int x) {  
+	if ( x == 0 ) { // base case  
+	    System.out.println(x); 
+	} else {
+	    System.out.println(x); 	
+            x -= 1;  	
+	    toZero(x); 
+	}	    
+    }  
+}     
